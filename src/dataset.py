0b94ca34ba010e03413fe917e30bcacd0b9af888
Dylan Devereaux <dylan.dev@uky.edu>
2026-02-28 22:37:30 -0500

    Integrate DatasetBuilder with scripts/pull_weather_allstores.py data + other updates

--- src/dataset.py
+++ src/dataset.py
@@ -1,5 +1,6 @@
-from datetime import datetime
 import pandas as pd
+from pathlib import Path
+
 from .config import *
 from .demand_features import add_demand_features
 from .weather.meteostat_client import get_daily_weather, WeatherConfig
@@ -10,16 +11,19 @@ store_info_cols = list(store_info.columns)
 
 perf = pd.read_csv(DATA_RAW / "store_performance_2018to2022.csv")
 perf["invoice_date"] = pd.to_datetime(perf["invoice_date"]).dt.normalize()
-perf_cols = list(perf.columns)
+perf_cols = ("invoice_count", "oc_count", "fleet_oc_count")
 
 class DatasetBuilder:
     # many, but not all of the features that have been used for training prototypes
-    # some are not fully implemented; more on that in the comments for the features setter
     location_features = list(set(store_info_cols) - {"invoice_date", "store_id"})
     calendar_features = ["year", "month", "day_of_year", "dow", "is_weekend"]
-    demand_features = ["inv_lag_1", "inv_lag_7", "inv_roll7_mean", "inv_roll14_mean", "inv_roll28_mean"]
+    demand_features = ["inv_lag_1", "inv_lag_7", "inv_rollmean_7", "inv_rollmean_14", "inv_rollmean_28"]
     weather_features = list(WeatherConfig.keep_cols)
     default_features = calendar_features + demand_features + weather_features
+
+    @property
+    def all_features(self):
+        return location_features + calendar_features + demand_features + weather_features
     
     def __init__(self, *, cutoff="2022-01-01", features=default_features, n_stores=None, store_selectors={}):
         self.cutoff = cutoff
@@ -38,16 +42,13 @@ class DatasetBuilder:
     @property
     def valid(self):
         return self.merged[self.merged["invoice_date"] >= self.cutoff].reset_index(drop=True).copy()
+
     
     def select_stores(self, n_stores=None, columns={}):
-        store_filter = store_info.store_id == store_info.store_id
-        store_ids = []
-
+        store_filter = pd.Series([True] * len(store_info.store_id))
         for col_name in columns:
             if col_name not in store_info_cols:
                 raise LookupError(f"Column '{col_name}' not in store_info.csv")
-            elif not isinstance(columns[col_name], (str, int, float, list, tuple, set)):
-                raise TypeError(f"Invalid type for argument '{col_name}={columns[col_name]}'")
 
             try:
                 store_filter &= store_info[col_name].isin(columns[col_name])
@@ -59,6 +60,10 @@ class DatasetBuilder:
 
         self.store_ids = store_ids
 
+        # trim dataframe if method is called after initialization
+        if hasattr(self, "merged"):
+            self.merged = self.merged[self.merged.store_id.isin(self.store_ids)].reset_index(drop=True)
+
     @property
     def features(self):
         return self._features
@@ -67,33 +72,46 @@ class DatasetBuilder:
     def features(self, features):
         self._features = list(features)
 
-        # location features
-        location_cols = list(set(features).intersection(self.location_features))
-        self.merged = self.merged.merge(store_info[["store_id", *location_cols]], how="left")
+        m = self.merged[["store_id", "invoice_date", *perf_cols]].copy()
 
+        # location features
+        location_cols = [c for c in features if c in self.location_features]
+        if len(location_cols) != 0:
+            m = m.merge(store_info[["store_id", *location_cols]], on=["store_id"], how="left")
+        
         # demand features
-        # due to how add_demand_features works, will insert all columns regardless of selection
-        has_demand_features = any(map(lambda f: f in self.demand_features, features))
-        if has_demand_features:
-            self.merged = add_demand_features(self.merged)
+        demand_cols = [c for c in features if c.startswith(("inv_lag_", "inv_rollmean_"))]
+        m = add_demand_features(m, demand_cols)
 
         # weather features
-        # currently uses ./weather/meteostat_client.py functionality
-        # but could be modified to function with data produced by scripts/pull_weather_allstores.py
-        weather_cols = tuple(set(features).intersection(self.weather_features))
-        weather_df = pd.DataFrame(columns=["store_id", "invoice_date", *weather_cols])
+        weather_cols = tuple(f for f in features if f in self.weather_features)
+        weather_df = self.get_weather_df(weather_cols)
+        m = m.merge(weather_df, on=["store_id", "invoice_date"], how="inner")
+        m = add_weather_features(m) # includes dow, month, and is_weekend
+
+        self.merged = m
+
+    def get_weather_df(self, cols=WeatherConfig.keep_cols):
+        # pull data outputted from scripts/pull_weather_allstores.py if it exists
+        allstores_path = Path(DATA_PROCESSED / "weather_allstores.parquet")
+        if allstores_path.exists():
+            weather_df = pd.read_parquet(allstores_path)
+            weather_df = weather_df[["store_id", "invoice_date", *cols]]
+            return weather_df
+
+        # otherwise, fetch from Meteostat using ./weather/meteostat_client.py
+        weather_df = pd.DataFrame(columns=["store_id", "invoice_date", *cols])
+        start = str(self.merged.invoice_date.min())
+        end = str(self.merged.invoice_date.max())
         for sid in self.store_ids:
             store = store_info[store_info.store_id == sid].iloc[0]
             lat = store.store_latitude
             lon = store.store_longitude
-            store_weather = get_daily_weather(str(sid), lat, lon,
-                                              str(self.merged.invoice_date.min()), str(self.merged.invoice_date.max()),
-                                              WeatherConfig(keep_cols=weather_cols))
+            store_weather = get_daily_weather(str(sid), lat, lon, start, end, WeatherConfig(keep_cols=weather_cols))
             store_weather["store_id"] = store_weather["store_id"].astype(int)
             weather_df = weather_df.merge(store_weather, how="outer")
 
-        self.merged = self.merged.merge(weather_df, on=["store_id", "invoice_date"], how="left")
-        self.merged = add_weather_features(self.merged) # includes dow, month, and is_weekend
+        return weather_df
 
     def to_csv(self, filename=None):
         if filename == None:
@@ -102,5 +120,9 @@ class DatasetBuilder:
         train_name = "train_" + filename
         valid_name = "valid_" + filename
 
-        self.train.to_csv(DATA_PROCESSED / train_name)
-        self.valid.to_csv(DATA_PROCESSED / valid_name)
+        if filename.endswith(".parquet"):
+            self.train.to_parquet(DATA_PROCESSED / train_name, index=False)
+            self.valid.to_parquet(DATA_PROCESSED / valid_name, index=False)
+        else:
+            self.train.to_csv(DATA_PROCESSED / train_name, index=False)
+            self.valid.to_csv(DATA_PROCESSED / valid_name, index=False)
--- src/demand_features.py
+++ src/demand_features.py
@@ -1,26 +1,24 @@
 import pandas as pd
 
-def add_demand_features(df: pd.DataFrame):
+default_demand_features = ["inv_lag_1", "inv_lag_7", "inv_rollmean_7", "inv_rollmean_14", "inv_rollmean_28"]
+
+def add_demand_features(df: pd.DataFrame, features=default_demand_features):
     out = df.copy()
     out["invoice_date"] = pd.to_datetime(out["invoice_date"]).dt.normalize()
     out = out.sort_values(["store_id", "invoice_date"]).reset_index(drop=True)
 
-    g = out.groupby("store_id", group_keys=False)
+    if features == []:
+        return out
+    
+    g = out.groupby("store_id", group_keys=False)["invoice_count"]
 
-    def lag(col: str, k: int):
-        if col in out.columns:
-            out[f"{col}_lag_{k}"] = g[col].shift(k)
+    lag_days = [int(f.removeprefix("inv_lag_")) for f in features if f.startswith("inv_lag_")]
+    rollmean_days = [int(f.removeprefix("inv_rollmean_")) for f in features if f.startswith("inv_rollmean_")]
 
-    def roll_mean(col: str, w: int):
-        if col in out.columns:
-            out[f"{col}_roll{w}_mean"] = g[col].shift(1).rolling(w).mean()
+    for i in lag_days:
+        out[f"inv_lag_{i}"] = g.shift(i)
 
-    lag("invoice_count", 1)
-    lag("invoice_count", 7)
-    lag("invoice_count", 14)
-
-    roll_mean("invoice_count", 7)
-    roll_mean("invoice_count", 14)
-    roll_mean("invoice_count", 28)
-
+    for i in rollmean_days:
+        out[f"inv_rollmean_{i}"] = g.shift(1).rolling(i).mean()
+        
     return out
